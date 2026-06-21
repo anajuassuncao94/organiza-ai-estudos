7a0ebc8fbb32221e89d9679cb632df404b88fe6d
anajuassuncao94 <anajulia.academica@gmail.com>
2026-06-21 18:17:41 -0300

    Add editar_disciplina function to update disciplines

--- disciplinas.py
+++ disciplinas.py
@@ -10,6 +10,8 @@ def adicionar_disciplina(usuario, nome, prioridade, horas_semana):
 def obter_disciplinas(usuario):
     return database.listar_disciplinas(usuario)
 
+def editar_disciplina(disciplina_id, nome, prioridade, horas_semana):
+    database.atualizar_disciplina(disciplina_id, nome, prioridade, horas_semana)
 
 def excluir_disciplina(disciplina_id):
     database.remover_disciplina(disciplina_id)
